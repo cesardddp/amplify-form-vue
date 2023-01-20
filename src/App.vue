8e22b0eb206ffebced9fea7a7fed3e4ec4789bf2
cesardddp <cesardddp@hotmail.com>
2023-01-20 10:27:42 -0300

    add pagina teste add syncfusion dependencias faltantes

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// import awsconfig from './old.js';
+import AmplifyFormVue from "./components/AmplifyForm.vue";
+import { AmplifyFormProps } from "./components/types";
+import schema from "./example/schema.json";
+const props: AmplifyFormProps = {
+  graphQLJSONSchema: schema,
+  entity: 'Campo',
+  onSubmit: () => console.log('submit'),
+}
+const form_refs = (e:any)=>console.log(e);
+
+</script>
+
+<template>
+      <AmplifyFormVue :form_props="props" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,39 @@
+import { createApp } from 'vue'
+// import './style.scss'
+
+//
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/scss/bootstrap.scss";
+import "bootstrap-icons/font/bootstrap-icons.css"; //https://stackoverflow.com/questions/63216712/use-bootstrap-icons-with-npm
+
+import { TextBoxPlugin } from '@syncfusion/ej2-vue-inputs';
+import { CheckBoxPlugin } from '@syncfusion/ej2-vue-buttons';
+import { ButtonPlugin } from '@syncfusion/ej2-vue-buttons';
+import { registerLicense } from "@syncfusion/ej2-base";
+import { enableRipple } from '@syncfusion/ej2-base';
+import { NumericTextBoxPlugin } from "@syncfusion/ej2-vue-inputs";
+//
+
+
+import App from './App.vue'
+
+//
+
+//
+import lodash from 'lodash'
+
+// name is optional
+
+
+registerLicense(
+    "ORg4AjUWIQA/Gnt2VVhjQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0VgWH9ZdHFVQGhVUUM="
+);
+enableRipple(true);
+
+
+createApp(App)
+.use(TextBoxPlugin)
+    .use(CheckBoxPlugin)
+    .use(ButtonPlugin)
+    .use(NumericTextBoxPlugin)
+    .mount('#app')
