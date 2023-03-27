4397f3f9d26fcf1a458379705719864ea08c9ef6
cesardddp <cesardddp@hotmail.com>
2023-03-27 16:07:00 -0300

    to build

--- src/AppToTest.vue
+++ src/AppToTest.vue
@@ -1,50 +1,7 @@
+<script setup lang="ts">
+import testForm from "./Form/tests/testForm.vue";
+</script>
+
 <template>
   <testForm />
-  <!-- <Tabela :colunas="colunas" :opcoes="tabelaOpcoes" :tela="'teste'" :api="api" :queries="queries"       :mutations="mutations"/> -->
 </template>
-<script setup lang="ts">
-// import awsconfig from './old.js';
-import testForm from "./Form/tests/testForm.vue";
-
-// const v = reactive(
-//   {
-//     "nome": "a",
-//     "label": "a",
-//     "tipo": "str",
-//     "visivel": true,
-//     "editavel": true,
-//     "listavel": true,
-//     "descricacao": "a",
-//     "mascara": "a",
-//     "tamanho": -1,
-//     "posicao": 1,
-//     "chave_primaria": false,
-//     "chave_extrangeira": false,
-//     "obrigatorio": true,
-//     "comentario": " a",
-//     "unique": true,
-//     "maxLength": 0,
-//     "minLength": 0,
-//     "formType": 'color',
-//     "items": [
-//       "a",
-//       "b"
-//     ],
-//     "default": "a",
-//     "modelCamposId": undefined,
-//     "chave primaria": true,
-//     "chave extrangeira": true,
-//     "maxlength": 1,
-//     "minlength": 1,
-//     "formtype": "text"
-//   }
-// )
-// const form_refs = (e: any) => console.log(e);
-// const tabelaOpcoes: GridTypes.OpcoesTabela = {}
-// const colunas: GridTypes.ColunaConfigs[] = [
-//   { label: 'label2', nome: 'nome1', posicao: 0, },
-//   { label: 'label2', nome: 'nome2', posicao: 1, },
-//   { label: 'label3', nome: 'nome3', posicao: 2, },
-// ]
-</script>
-<style scoped></style>
