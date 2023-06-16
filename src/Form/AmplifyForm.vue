23ee157c434338b24b38370c42cf7240f4cf7050
cesardddp <cesardddp@hotmail.com>
2023-06-16 16:42:13 -0300

    refac AmplifyFormVue, expose validacao

--- src/Form/AmplifyForm.vue
+++ src/Form/AmplifyForm.vue
@@ -1,76 +1,59 @@
-<script lang="ts">
+<script setup lang="ts">
 import { IntrospectionSchema } from "../introspectionSchemaInterface";
-import { defineComponent, provide, type PropType, reactive, ref, watch, computed } from "vue";
+import { defineComponent, provide, type PropType, reactive, ref, watch, computed, onMounted } from "vue";
 import IntrospectionParser from "./parse-introspection";
-import { FormStateHandler, FormStylingHandler } from "./formStorage";
+import { FormStateHandler, FormStylingHandler, FormsValidation } from "./formStorage";
 import FormHandler from "./FormHandler.vue";
-import { Validacao } from "./formTypes";
 
-export default defineComponent({
-    props: {
-        introspectionSchema: { type: Object as PropType<IntrospectionSchema>, required: true },
-        entity_name: { type: String, required: true },
-        modelValue: {
-            type: Object, default: () => ({}),
-        },
-        validar: { type: Boolean },
-        form_id: { type: String }
+interface AmplifyForm {
+    introspectionSchema: IntrospectionSchema,
+    entity_name: string,
+    modelValue: {
+        type: Object, default: () => ({}),
     },
-    emits: ['update:modelValue', 'validado'],
-    components: {
-        FormHandler
+    form_id: string
+}
+
+const props = defineProps<AmplifyForm>()
+const emits = defineEmits(['update:modelValue'])
+
+const form_state_handler = new FormStateHandler(
+    emits,
+    reactive({ root: props.modelValue })
+)// inicializa o gerenciador de estado global do amplify form
+
+const form_types = IntrospectionParser(
+    props.entity_name,
+    props.introspectionSchema
+)
+
+const status = ref<'parado' | 'atualizando' | 'erro' | 'sucesso'>('parado')
+const status_form_style = {
+    set_status: (status_: 'parado' | 'atualizando' | 'erro' | 'sucesso') => {
+        status.value = status_
+        status.value != 'atualizando' && setTimeout(() => {
+            status.value = 'parado'
+        }, 2000)
     },
-    setup(props,
-        { emit }
-    ) {
-
-        const form_state_handler = new FormStateHandler(
-            emit,
-            reactive({ root: props.modelValue }))// inicializa o gerenciador de estado global do amplify form
-
-        const form_types = IntrospectionParser(
-            props.entity_name,
-            props.introspectionSchema
-        )
-
-        const status = ref<'parado' | 'atualizando' | 'erro' | 'sucesso'>('parado')
-        const status_form_style = {
-            set_status:(status_: 'parado' | 'atualizando' | 'erro' | 'sucesso') =>{
-                status.value = status_
-                status.value != 'atualizando' && setTimeout(() => {
-                    status.value = 'parado'
-                }, 2000)
-            },
-        }
+}
 
 
-        provide("form_state_handler", form_state_handler)
-        provide("form_types", form_types)
-        provide("form_styling_handler", new FormStylingHandler(props.form_id!, status_form_style.set_status))
+provide("form_state_handler", form_state_handler)
+provide("form_types", form_types)
+provide("form_styling_handler", new FormStylingHandler(props.form_id!, status_form_style.set_status))
 
-        const validacao = reactive<Validacao>({
-            trigger_validacao: computed(() => props.validar),
-            validado: true,
-            validacoes: []
-        })
-        provide("validacao", validacao)
-        watch(
-            validacao,
-            () => {
-                if (validacao.validado) {
-                    emit('validado', validacao.validado)
-                }
-            }
-        )
+const validacao = new FormsValidation()
+defineExpose(
+    { validacao }
+)
+provide("validacao", validacao)
 
 
-        return {
-            input_nome: form_types.keys().next().value, status_form_style,status
-        }
-    }
-})
+// input_nome: form_types.keys().next().value, status_form_style,status
+
 </script>
 <template>
+    {{ validacao.state }}
     <section v-if="!(status === 'parado')" style="font-size:small">
         <span v-if="status === 'atualizando'" class="text-info text-opacity-75">
             <div class="spinner-grow spinner-grow-sm " role="status">
@@ -86,6 +69,6 @@ export default defineComponent({
             Form atualizado com sucesso!
         </span>
     </section>
-    <FormHandler :introspection_caminho="'root'" :form_name="input_nome" :field_name="input_nome"
+    <FormHandler :introspection_caminho="'root'" :form_name="form_types.keys().next().value" :field_name="form_types.keys().next().value"
         :is_multipleform_item="false" />
 </template>
